75bdf0c91d1a8b22bea2e7326cae3aebca48347e
Alex Cluff <alexcluff16@gmail.com>
2022-03-19 01:59:36 -0400

    working bar model

--- training/platesClassCenterModel.py
+++ training/platesClassCenterModel.py
@@ -1,6 +1,4 @@
 # %%
-from curses import noecho
-from matplotlib.animation import ImageMagickBase
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -35,8 +33,9 @@ class PlatesData:
         self.center = torch.tensor(np.array(self.df[['xmin', 'ymin']], dtype=np.float32), device='cpu')
         self.labels = torch.tensor(np.array(pd.get_dummies(self.df['label']), dtype=np.float32), device='cpu')
 
-    def process(self, img):
-        img = transforms.RandomGrayscale(0.25)(img)
+    def process(self, img, train = True):
+        if train:
+            img = transforms.RandomGrayscale(0.5)(img)
         img = transforms.ToTensor()(img)
         img = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])(img)
         return img
@@ -74,7 +73,7 @@ test = plate_data[:4]
 
 train_dl = DataLoader(
     plate_data,
-    batch_size=256,
+    batch_size=512,
     shuffle=True
 )
 valid_dl = DataLoader(
@@ -149,9 +148,9 @@ def train_epocs(model, optimizer, train_dl, val_dl, epochs=10,C=1000):
             sum_loss += loss.item()
         train_loss = sum_loss/total
         #val_loss, val_acc = val_metrics(model, valid_dl, C)
-        val_loss = val_metrics(model, valid_dl, C)
+        #val_loss = val_metrics(model, valid_dl, C)
         #print("train_loss %.3f val_loss %.3f val_acc %.3f" % (train_loss, val_loss, val_acc))
-        print(i, "train_loss %.3f val_loss %.3f" % (train_loss, val_loss))
+        print(f'epoch {i} train_loss {train_loss}')
     return sum_loss/total
 # %%
 model = PlateModel().cuda()
@@ -191,9 +190,8 @@ def add_points(image: Image = None, fp: str = None, center = None, label = None)
     #image = image.resize((512, 512))
 
     draw = ImageDraw.Draw(image)
-    draw.ellipse((x-10, y-10, x+10, y+10), fill=(255,0,0,0))
+    draw.ellipse((x-3, y-3, x+3, y+3), fill=(255,0,0,0))
     draw.text((0., 0.), text = label)
-    print(x,y)
     return image
 
 override = True
@@ -201,7 +199,7 @@ if __name__ == "__main__" and not override:
     try:
         model.load_state_dict(torch.load(f'{DIR_PATH}/../models/plate_model.pth'))
         model = model.cuda()
-        train_epocs(model, optimizer, train_dl, valid_dl, epochs=100)
+        train_epocs(model, optimizer, train_dl, valid_dl, epochs=250)
     except KeyboardInterrupt:
         pass
     torch.save(model.state_dict(), f'{DIR_PATH}/../models/plate_model.pth')
@@ -210,7 +208,7 @@ else:
     model = model.cuda()
 
 # %%
-for i in range(7):
-    print(get_prediction(fp=f'/root/workspace/bar_tracking/training/test{i+1}.jpg'))
+for i in range(8):
+    #print(get_prediction(fp=f'/root/workspace/bar_tracking/training/test{i+1}.jpg'))
     add_points(fp=f'/root/workspace/bar_tracking/training/test{i+1}.jpg').save(f'/root/workspace/bar_tracking/training/pred{i+1}.jpg')
 # %%
