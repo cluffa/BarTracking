7b4e76c15ccbf9d0983eb076409c7c080d20ad1a
Alex Cluff <alexcluff16@gmail.com>
2022-04-28 01:00:14 -0400

    fix

--- setup.py
+++ setup.py
@@ -1,16 +1,4 @@
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup(
-        setup_resuires = ['torch'],
-        install_requires = [
-            "setuptools",
-            "numpy",
-            "scipy",
-            "pandas",
-            "scikit-image",
-            "torch",
-            "torchvision",
-            "opencv-python"
-            ],
-    )
+    setuptools.setup()
