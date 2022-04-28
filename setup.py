f9af39f13981c2963507ae4e08798f088cd33f95
Alex Cluff <alexcluff16@gmail.com>
2022-04-28 00:44:56 -0400

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ import setuptools
 
 if __name__ == "__main__":
     setuptools.setup(
-        install_requires=[
+        install_requires = [
+            "setuptools",
             "numpy",
             "scipy",
             "pandas",
