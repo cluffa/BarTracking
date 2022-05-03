74645e7c86f911a37251629285470dfefc59b0d2
Alex Cluff <alexcluff16@gmail.com>
2022-05-03 19:48:09 -0400

    fix

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup()
+    setuptools.setup(
+        name = 'bar-tracking',
+        version = 0.2,
+        author = 'cluffa',
+        author_email = 'alexcluff16@gmail.com',
+        url = 'https://github.com/cluffa/bar_tracking',
+        packages=setuptools.find_packages(),
+        install_requires = [
+            'numpy', 'scipy', 'pandas', 'scikit-image', 'torch', 'torchvision', 'opencv-python', 'matplotlib'
+        ]
+    )
