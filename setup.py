ae7360976d764eb2990cf76e31e654ad29b6a755
Alex Cluff <alexcluff16@gmail.com>
2022-05-03 20:20:21 -0400

    fix

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 if __name__ == "__main__":
     setuptools.setup(
-        name = 'bar-tracking',
+        name = 'BarTracking',
         version = 0.2,
         author = 'cluffa',
         author_email = 'alexcluff16@gmail.com',
         url = 'https://github.com/cluffa/bar_tracking',
         packages = ['BarTracking'],
-        package_data={'': ['BarTracking/best_model.pth']},
+        package_data={'BarTracking': ['*.pth']},
         install_requires = [
             'numpy', 'scipy', 'pandas', 'torch', 'torchvision', 'opencv-python', 'matplotlib'
         ]
