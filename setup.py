1ff5c0105ee8b9c2a1a8c764a5665ca95b123de0
Alex Cluff <alexcluff16@gmail.com>
2022-05-03 20:16:59 -0400

    manifest into setup

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
         author_email = 'alexcluff16@gmail.com',
         url = 'https://github.com/cluffa/bar_tracking',
         packages = ['BarTracking'],
+        package_data={'': ['BarTracking/best_model.pth']},
         install_requires = [
             'numpy', 'scipy', 'pandas', 'torch', 'torchvision', 'opencv-python', 'matplotlib'
         ]
