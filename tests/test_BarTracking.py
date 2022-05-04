cd75bff33a9e1c8761f6f8ba69cdcfa036a84fcf
Alex Cluff <alexcluff16@gmail.com>
2022-05-03 21:41:21 -0400

    add tests

---
+++ tests/test_BarTracking.py
@@ -0,0 +1,39 @@
+from BarTracking.track import *
+import os
+
+dir = os.path.dirname(os.path.abspath(__file__))
+
+def test_load():
+    try:
+        Track(video_fp = dir + '/test.mp4')
+        assert True
+    except Exception as e:
+        print(e)
+        assert False
+    
+def test_process():
+    try:
+        t = Track(video_fp = dir + '/test.mp4')
+        t.process_video()
+        assert True
+    except Exception as e:
+        print(e)
+        assert False
+    
+def test_run():
+    try:
+        t = Track(video_fp = dir + '/test.mp4')
+        t.run()
+        assert True
+    except Exception as e:
+        print(e)
+        assert False
+    
+def test_graph():
+    try:
+        t = Track(video_fp = dir + '/test.mp4')
+        plot_trajectory(t, dir + '/.test.png')
+        assert True
+    except Exception as e:
+        print(e)
+        assert False
