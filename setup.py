635d99e8234eacccb54f5f27395180d932f87993
Alex Cluff <alexcluff16@gmail.com>
2022-05-04 21:13:31 -0400

    fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         url = 'https://github.com/cluffa/bar_tracking',
         packages = ['BarTracking'],
         package_dir={'BarTracking': 'BarTracking'},
-        package_data={'BarTracking': ['*.onnx']},
+        package_data={'BarTracking': ['*.onnx', '*.json']},
         test_suite = 'tests',
         install_requires = [
             'numpy',
@@ -17,8 +17,7 @@ if __name__ == "__main__":
             'pandas',
             'onnxruntime-gpu',
             'opencv-python',
-            'matplotlib',
-            'segmentation-models-pytorch'
+            'matplotlib'
         ],
     )
     
