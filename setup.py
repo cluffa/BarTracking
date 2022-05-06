e465bbf0ab0eb8cd1bd360fe93c28b6fba071e17
Alex Cluff <alexcluff16@gmail.com>
2022-05-05 20:58:53 -0400

    ver

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 if __name__ == "__main__":
     setuptools.setup(
         name = 'BarTracking',
-        version = 0.4,
+        version = 0.5,
         author = 'cluffa',
         author_email = 'alexcluff16@gmail.com',
         url = 'https://github.com/cluffa/bar_tracking',
